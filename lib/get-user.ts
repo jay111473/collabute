dbf2d9dcdb5acb462acb768a8e5686d56aa9f913
Mup <85489543+muperdev@users.noreply.github.com>
2025-03-16 21:50:11 +0400

    feat: github sign in flow

--- lib/get-user.ts
+++ lib/get-user.ts
@@ -1,11 +1,10 @@
 import { User } from "@/types/dashboard";
 import axios from "axios";
 
-export async function getUser(id: string, token: string): Promise<User> {
-  
+export async function getUser(token: string): Promise<any> {
   try {
     const response = await axios.get<User>(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/users/${id}`,
+      `${process.env.NEXT_PUBLIC_API_URL}/api/users/me`,
       { headers: { Authorization: `Bearer ${token}` } }
     );
     return response?.data;

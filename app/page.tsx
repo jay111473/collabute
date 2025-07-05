b748afd3f3206310b9037716fb7d81339bdb07f3
Mup <85489543+muperdev@users.noreply.github.com>
2025-07-05 16:35:03 +0400

    fix: remove useisauthenticated in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,10 @@ import Developers from "@/components/home/developers";
 import HowItWorks from "@/components/home/how-it-works";
 import FAQ from "@/components/home/faq";
 import Footer from "@/components/footer";
-import { useIsAuthenticated } from "./auth/hooks/use-is-authenticated";
 import Image from "next/image";
 import CountdownTimer from "@/components/home/countdown-timer";
 
 export default async function Home() {
-  const isAuthenticated = await useIsAuthenticated();
   return (
     <>
       <div className="relative h-full min-h-screen w-full">
